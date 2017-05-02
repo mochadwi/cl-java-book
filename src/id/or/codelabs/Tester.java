4b3d68215c475b9889f603890a01f8493478657b
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-02 16:07:38 +0700

    [Add] displayName with String return type

--- src/id/or/codelabs/Tester.java
+++ src/id/or/codelabs/Tester.java
@@ -1,15 +1,12 @@
 package id.or.codelabs;
 
-import id.or.codelabs.model.Karyawan;
-import id.or.codelabs.model.Manusia;
-
 public class Tester {
     public static void main(String args[]) {
-        //Memunculkan Hello, world berulang kali
-        int i = 1;
-        do {
-            System.out.println(i + ". Hello, world!");
-            i++;
-        } while (i <= 5);
+        System.out.println(displayName("Doni"));
+    }
+
+    //Prosedur
+    private static String displayName(String name) {
+        return "Hello, " + name + "!";
     }
 }
