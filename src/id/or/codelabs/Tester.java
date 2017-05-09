aab6a6100ee5fd5a6a63601c97cf91c59d07d7f7
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-09 19:56:06 +0700

    [Add] Chapter #02 - Array case

--- src/id/or/codelabs/Tester.java
+++ src/id/or/codelabs/Tester.java
@@ -1,10 +1,26 @@
 package id.or.codelabs;
 
 public class Tester {
-    public static void main(String args[]) {
-      String hello = "Hello, ";
-      String world = "world!";
-      System.out.println( "Ini menghasilkan : " + hello.equals(world));
-      System.out.println( "Ini menghasilkan : " + hello.equals(hello));
+    public static void main(String[] args) {
+      double[] larik = {1.9, 2.9, 3.4, 3.5};
+
+      // Tampilkan elemen dari larik
+      for (int i = 0; i < larik.length; i++) {
+         System.out.println(larik[i] + " ");
+      }
+     
+      // Menjumlahkan semua elemen dalam larik
+      double total = 0;
+      for (int i = 0; i < larik.length; i++) {
+         total += larik[i];
+      }
+      System.out.println("Nilai total: " + total);
+      
+      // Mencari nilai terbesar dalam larik
+      double maks = larik[0];
+      for (int i = 1; i < larik.length; i++) {
+         if (larik[i] > maks) maks = larik[i];
+      }
+      System.out.println("Nilai maksimum: " + maks);
    }
 }
