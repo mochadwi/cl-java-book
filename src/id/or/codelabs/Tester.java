4a74a8666846750738e2223cf38ba4c53ffd636a
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-09 15:24:06 +0700

    [Add] Chapter #02 - (String) Concat function case

--- src/id/or/codelabs/Tester.java
+++ src/id/or/codelabs/Tester.java
@@ -2,8 +2,11 @@ package id.or.codelabs;
 
 public class Tester {
     public static void main(String args[]) {
-      String palindrome = "Dot saw I was Tod";
-      int len = palindrome.length();
-      System.out.println( "String Length is : " + len );
+      String hello = "Hello, ";
+      String world = "world!";
+      System.out.println( "This is : " + hello.concat(world));
+
+      String name = "Doni!";
+      System.out.println( "This is : " + hello + "you are " + name);
    }
 }
