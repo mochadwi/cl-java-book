84a922538ad2f5a8c663d0ba8dc56c2d8e935bf8
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-17 18:54:31 +0700

    [Update] Chapter #09 - Exception handling message, now desribe more clearly

--- src/id/or/codelabs/Tester.java
+++ src/id/or/codelabs/Tester.java
@@ -1,11 +1,13 @@
+package id.or.codelabs;
+
 public class Tester {
     public static void main(String[] args) {
       try {
          int a[] = new int[2];
-         System.out.println("Access element three :" + a[3]);
+         System.out.println("Mengakses elemen di indeks 3, hasilnya: " + a[3]);
       } catch(ArrayIndexOutOfBoundsException e) {
-         System.out.println("Exception thrown  :" + e);
+         System.out.println("Exception yang dimunculkan \"" + e.toString() + "\"");
       }
-      System.out.println("Out of the block");
+      System.out.println("Di luar block \"try and catch\"");
    }
 }
