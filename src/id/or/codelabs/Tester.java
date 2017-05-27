7ece3dd73a7919992c807a843588cc32be4f180b
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-27 15:00:05 +0700

    [Update] Chapter #09 - NullPointerException add additional Mahasiswa object

--- src/id/or/codelabs/Tester.java
+++ src/id/or/codelabs/Tester.java
@@ -1,10 +1,23 @@
 package id.or.codelabs;
 
 public class Tester {
+    static class Mahasiswa {
+        static String name;
+
+        public Mahasiswa() {
+            name = null;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
     public static void main(String[] args) {
       try {
-         Object obj = null;
-         obj.toString();
+          Mahasiswa doni = new Mahasiswa();
+          doni.name.toString();
       } catch(NullPointerException e) {
          System.out.println("Exception yang dimunculkan \"" + e.toString() + "\"");
       }
