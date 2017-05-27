199e926983a2f16fd995323376dd51ba1582157e
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-27 14:22:19 +0700

    [Update] Chapter #09 - NullPointerException

--- src/id/or/codelabs/Tester.java
+++ src/id/or/codelabs/Tester.java
@@ -3,9 +3,9 @@ package id.or.codelabs;
 public class Tester {
     public static void main(String[] args) {
       try {
-         int a[] = new int[2];
-         System.out.println("Mengakses elemen di indeks 3, hasilnya: " + a[3]);
-      } catch(ArrayIndexOutOfBoundsException e) {
+         Object obj = null;
+         obj.toString();
+      } catch(NullPointerException e) {
          System.out.println("Exception yang dimunculkan \"" + e.toString() + "\"");
       }
       System.out.println("Di luar block \"try and catch\"");
